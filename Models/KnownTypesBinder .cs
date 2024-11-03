884461d810349a2891ba44247a8977d0e197fcc8
Piotr Kostecki <95875856+KaleIsProgramming@users.noreply.github.com>
2024-11-03 12:57:35 +0100

    Feat: created data logic.

---
+++ Models/KnownTypesBinder .cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_PO.Models
+{
+    public class KnownTypesBinder : ISerializationBinder
+    {
+        public IList<Type> KnownTypes { get; set; }
+
+        public Type BindToType(string assemblyName, string typeName)
+        {
+            return KnownTypes.SingleOrDefault(t => t.Name == typeName);
+        }
+
+        public void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            if (KnownTypes.Contains(serializedType))
+            {
+                assemblyName = null;
+                typeName = serializedType.Name;
+            }
+            else
+            {
+                assemblyName = null;
+                typeName = null;
+            }
+        }
+    }
+}
---
+++ Models/MediaDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using PP_PO.Models;
+
+public static class MediaDataAccess
+{
+    private static string filePath = "media.json";
+    private static JsonSerializerSettings settings = new JsonSerializerSettings
+    {
+        TypeNameHandling = TypeNameHandling.Auto,
+        SerializationBinder = new KnownTypesBinder
+        {
+            KnownTypes = new List<Type>
+            {
+                typeof(ActionFilm),
+                typeof(Book),
+                typeof(EBook),
+                typeof(MusicAlbum),
+                typeof(VinylRecord),
+                // Dodaj inne typy pochodne
+            }
+        }
+    };
+
+    public static void SaveMediaList(List<Media> mediaList)
+    {
+        string json = JsonConvert.SerializeObject(mediaList, Newtonsoft.Json.Formatting.Indented, settings);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static List<Media> LoadMediaList()
+    {
+        if (!File.Exists(filePath))
+        {
+            return new List<Media>();
+        }
+        string json = File.ReadAllText(filePath);
+        List<Media> mediaList = JsonConvert.DeserializeObject<List<Media>>(json, settings);
+        return mediaList;
+    }
+}
